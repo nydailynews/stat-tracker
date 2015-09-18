07830d57f4d888e9106270fef085574334c653d9
freejoe76 <jmurphy@denverpost.com>
2015-09-18 16:15:46 -0400

    Start pulling in per-player sacks

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source source.bash
-python stat.py  broncos-sacks-2015 broncos-manning-passing-yards-2015 
+python stat.py  broncos-sacks-2015 broncos-sacks-by-player-2015 broncos-manning-passing-yards-2015 
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
